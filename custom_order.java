34b35e7571b10fe3555cd0e9cd58276795293824
Helen <31750014+hxgreensoul@users.noreply.github.com>
2019-07-28 11:18:44 -0700

    Add files via upload

---
+++ custom_order.java
@@ -0,0 +1,89 @@
+// Code for Beginning Java's Cake & Cupcake Shop Tutorial
+
+import java.util.Scanner;  // Needed for the Scanner class to read input
+
+public class custom_order {
+
+    // STEP 1 PRINTING HELLO WORLD TO CONSOLE
+    public static void main(String[] args) {
+        
+      System.out.println("Hello World!"); // print Hello World to console
+        
+    // TEST CODE
+    
+    // STEP 2 CREATE A SCANNER OBEJCT, DECLARE VARAIBLES, & PRINT STATEMENTS
+	  
+
+	 
+    // Introduce shop and prompt user to input first name
+      
+
+	  
+	  
+    // TEST CODE
+    
+    // STEP 3 INPUT YOUR NAME AND PREPARE TO VIEW MENU
+   
+	  
+      
+    // TEST CODE     
+    
+    // STEP 4 DISPLAY MENU
+
+	
+      
+
+    // TEST CODE     
+    
+    // STEP 5 PROMPT USER TO ORDER
+
+  
+	  
+	  
+	  
+      
+    // TEST CODE
+      
+    // STEP 6 PROMPT USER TO CHOOSE FROSTING
+
+   
+	 
+	 
+	 
+      
+    //TEST CODE
+      
+    // STEP 7 PROMPT USER TO CHOOSE FILLING
+
+
+	  
+	  
+      
+    // TEST CODE
+      
+    // STEP 8 PROMPT USER TO CHOOSE TOPPINGS
+
+
+	
+      
+      
+    // TEST CODE
+      
+    // STEP 9 DISPLAY ORDER CONFIRMATION
+
+
+      
+	  
+	  
+	  
+    
+    // TEST CODE
+      
+    // STEP 10 DISPLAY COST AND SALES TAX
+
+	  
+	  
+	  
+	  
+    }   
+}
